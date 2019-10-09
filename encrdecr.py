15e3724906df56e5147985e45fcedfec452afc5f
cWjL <jacobloden@hotmail.com>
2019-10-09 15:02:22 -0700

    finished decrypt

--- encrdecr.py
+++ encrdecr.py
@@ -14,13 +14,24 @@ def main():
     '''
     parser = argparse.ArgumentParser()
     parser.add_argument('-g','--genkey',action='store_true',dest='genkey',help='Generate new random key')
-    parser.add_argument('-p','--passwd',action='store',dest='passwd',help='Generate key from password')
+    parser.add_argument('-p','--passwd',action='store',dest='passwd',help='Use password based key')
     parser.add_argument('-e','--encrypt',action='store',dest='encr',help='Encrypt this file or string')
     parser.add_argument('-d','--decrypt',action='store',dest='decr',help='Decrypt this file or string')
     parser.add_argument('-k','--key',action='store',dest='key',help='Use this key')
-    parser.add_argument('-w','--passwd-key',action='store',dest='passkey',help='Decrypt with this password')
 
     args = parser.parse_args()
+    
+    '''
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+
+    b_prefix = "["+RED+"FAIL"+ENDC+"] "
+    g_prefix = "["+GREEN+" OK "+ENDC+"] "
+    n_prefix = "["+YELLOW+" ** "+ENDC+"] "
+    '''
     
     try:
         import colorama
@@ -36,109 +47,132 @@ def main():
 
     prefixes = [b_prefix, g_prefix, n_prefix]
     
-    ###################################################################################################
-    ##################################### DEBUG ######################################################
-    ##################################################################################################
-    # Testing creating key with password then decrypting it with the same password
-    if args.passwd and not (args.genkey or args.encr or args.decr):
+    if args.passwd and not (args.genkey or args.encr or args.decr or args.key):
+        
         _write_passwd_key(args.passwd, _gen_passwd_key(args.passwd), prefixes)
-    elif args.passwd and args.encr and not (args.genkey or args.decr):
+
+    elif args.genkey and not (args.passwd or args.encr or args.decr or args.key):
+
+        _write_gen_key(_gen_key())
+        
+    elif args.passwd and args.encr and not (args.genkey or args.decr or args.key):
+
         _key = _gen_passwd_key(args.passwd)
         _write_passwd_key(args.passwd, _key, prefixes)
-        try:
-            if os.path.exists(args.encr):                
-                with open(args.encr, 'rb') as in_file:
-                    _pt_data = in_file.read()
-                _encr_and_write(args.encr, _pt_data, _key, prefixes)
-            else:
-                raise FileNotFoundError
-        except FileNotFoundError:
-            print(prefixes[0]+"\'"+args.encr+"\'"+" file not found, I'm assuming it's a string")
-            _pt_data = args.encr
-            _encr_str(_pt_data.encode(), _key, prefixes)
-    elif args.decr and args.key and not (args.encr or args.passwd):
+        _encr_and_write(args.encr, _key, prefixes)
         
+    elif args.key and args.encr and not (args.passwd or args.decr or args.genkey):
         
-
-    sys.exit(0)
-
-    ##################################################################################################
-    ################################### / DEBUG ######################################################
-    ##################################################################################################
-    
-    if args.genkey and not (args.encr or args.decr or args.key or args.passwd):
-        # generate random key only
-        _write_gen_key(_gen_key(), prefixes)
-        
-    elif args.passwd and not (args.genkey or args.encr or args.decr):
-        # generate key from password
-        _write_passwd_key(args.passwd, _gen_passwd_key(args.passwd), prefixes)
-
-    elif args.genkey and args.encr and not (args.decr or args.passwd):
-        # generate random key, encrypt file and save
-        _key = _gen_key()
-        _write_gen_key(_key, prefixes)
-        try:
-            if os.path.exists(args.encr):                
-                with open(args.encr, 'rb') as in_file:
-                    _pt_data = in_file.read()
-                _encr_and_write(args.encr, _pt_data, _key, prefixes)
-            else:
-                raise FileNotFoundError
-        except FileNotFoundError:
-            print(prefixes[0]+"\'"+args.encr+"\'"+" file not found, I'm assuming it's a string")
-            _pt_data = args.encr
-            _encr_str(_pt_data.encode(), _key, prefixes)
+        _encr_and_write(args.encr, args.key, prefixes)
             
-    elif args.paswd and args.encr and not (args.decr or args.genkey):
-        # generate key from password, encrypt file and save
-        _key = _gen_passwd_key(args.passwd)
-        _write_passwd_key(args.passwd, _key, prefixes)
-        try:
-            if os.path.exists(args.encr):                
-                with open(args.encr, 'rb') as in_file:
-                    _pt_data = in_file.read()
-                _encr_and_write(args.encr, _pt_data, _key, prefixes)
-            else:
-                raise FileNotFoundError
-        except FileNotFoundError:
-            print(prefixes[0]+"\'"+args.encr+"\'"+" file not found, I'm assuming it's a string")
-            _pt_data = args.encr
-            _encr_str(_pt_data.encode(), _key, prefixes)
-    # TODO add decrypt functions
+    elif args.decr and args.key and not (args.encr or args.passwd or args.genkey):
+        
+        _decr_with_key(args.decr, args.key, prefixes)
+            
+    elif args.decr and args.passwd and not (args.encr or args.key or args.genkey):
+
+        _decr_with_passwd(args.decr, args.passwd, prefixes)
+        
     else:
-        # catch all
         parser.print_help()
-            
+
     sys.exit(0)
 
 def _decr_with_key(data, key, prefixes):
-    _fn = Fernet(key)
     try:
-        _decr_data = _fn.decrypt(data)
-        print(prefixes[1]+"Dencrypted string: "+_decr_data.decode())
-    except InvaidToken:
-        raise ValueError("Invalid Key!")
+        
+        if os.path.exists(key):
+            with open(key, 'r') as in_key:
+                _key = in_key.read()
+
+            _fn = Fernet(_key)
+        else:
+            raise ValueError
+        
+        if os.path.exists(data):
+            if ".encr" in data:
+                
+                with open(data, 'rb') as in_file:
+                    _pt_data = in_file.read()
+                    
+                _decr_data = _fn.decrypt(_pt_data)
+                _plain_file_name = data[:data.rfind(".")]
+
+                with open(_plain_file_name, 'wb') as out_file:
+                    out_file.write(_decr_data)
+
+                os.remove(data)
+                print(prefixes[1]+"Key provided: "+"\'"+key+"\'")
+                print(prefixes[1]+"File processed: "+"\'"+data+"\'")
+                print(prefixes[1]+"Decrypted file created: "+"\'"+_plain_file_name+"\'")
+            else:
+                TypeError
+        else:
+            print(prefixes[2]+"\'"+data+"\'"+" file not found, I'm assuming it's a string")
+            _decr_data = _fn.decrypt(data.encode())
+            print(prefixes[1]+"Key provided: "+"\'"+key+"\'")
+            print(prefixes[1]+"Decrypted string: "+_decr_data.decode())
+ 
+    except ValueError:
+        print(prefixes[0]+"\'"+key+"\'"+" does not exist")
+    except TypeError:
+        print(prefixes[0]+"\'"+data+"\'"+" was not encrypted using this program")
+    except InvalidToken:
+        print(prefixes[0]+"Invalid Key")
 
 def _decr_with_passwd(data, passwd, prefixes):
     _key = _gen_passwd_key(passwd)
     _fn = Fernet(_key)
+
     try:
-        _decr_data = _fn.decrypt(data)
-        print(prefixes[1]+"Dencrypted string: "+_decr_data.decode())
+        if os.path.exists(data):
+            if ".encr" in data:
+                
+                with open(data, 'rb') as in_file:
+                    _pt_data = in_file.read()
+                    
+                _decr_data = _fn.decrypt(_pt_data)
+                _plain_file_name = data[:data.rfind(".")]
+
+                with open(_plain_file_name, 'wb') as out_file:
+                    out_file.write(_decr_data)
+
+                os.remove(data)
+                print(prefixes[1]+"Key provided: "+"\'"+key+"\'")
+                print(prefixes[1]+"File processed: "+"\'"+data+"\'")
+                print(prefixes[1]+"Decrypted file created: "+"\'"+_plain_file_name+"\'")
+            else:
+                TypeError
+        else:
+            print(prefixes[2]+"\'"+data+"\'"+" file not found, I'm assuming it's a string")
+            _decr_data = _fn.decrypt(data.encode())
+            print(prefixes[1]+"Key provided: "+"\'"+passwd+"\'")
+            print(prefixes[1]+"Decrypted string: "+_decr_data.decode())
+    except TypeError:
+        print(prefixes[0]+"\'"+data+"\'"+" was not encrypted using this program")
     except InvalidToken:
-        raise ValueError("Invalid Password!")
+        print(prefixes[0]+"Invalid password")
 
-def _encr_str(data, key, prefixes):
+def _encr_and_write(in_file, key, prefixes):
     _fn = Fernet(key)
-    _encr_data = _fn.encrypt(data)
-    print(prefixes[1]+"Encrypted string: "+_encr_data.decode())
 
-def _encr_and_write(in_file, data, key, prefixes):
-    _fn = Fernet(key)
-    _encr_data = _fn.encrypt(data)
-    with open(in_file+".encr",'wb') as out_file:
-        out_file.write(_encr_data)
+    if os.path.exists(in_file):
+            
+        with open(in_file, 'rb') as file_in:
+            _pt_data = file_in.read()
+                
+        _encr_data = _fn.encrypt(_pt_data)
+            
+        with open(in_file+".encr",'wb') as out_file:
+            out_file.write(_encr_data)
+                
+        os.remove(in_file)
+            
+    else:
+        print(prefixes[0]+"\'"+in_file+"\'"+" file not found, I'm assuming it's a string")
+        _pt_data = in_file
+        _encr_data = _fn.encrypt(_pt_data.encode())
+        print(prefixes[1]+"Encrypted string: "+_encr_data.decode())
 
 def _write_passwd_key(passwd, pass_key, prefixes):
     _key_file = ""
